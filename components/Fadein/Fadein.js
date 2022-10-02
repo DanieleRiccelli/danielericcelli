72c4657b339eac094dfd0a6444fa97dce84d518b
danielericcelli <danielericcelli@me.com>
2022-10-02 13:11:22 +0200

    Hero

--- components/Fadein/Fadein.js
+++ components/Fadein/Fadein.js
@@ -2,9 +2,20 @@ import styles from "./Fadein.module.css"
 
 function Fadein() {
     return (
+        <>
         <div className={styles.container}>
-            <h1>WEEE </h1>
+            <div className={styles.hideMob}>
+                <h1 className={styles.pt1}>Ciao sono Daniele Riccelli<span style={{"opacity": "0"}}>.</span></h1>
+                <h1 className={styles.pt2}>e sono un Web Developer.<span className={styles.span}>|</span></h1>
+            </div>
+            <div className={styles.showMob}>
+                <h1 className={styles.pt1}>Ciao sono Daniele Riccelli<span style={{"opacity": "0"}}>.</span></h1>
+                <h1 className={styles.pt2}>e sono un Web Developer.<span className={styles.span}>|</span></h1>
+            </div>
+
         </div>
+
+        </>
     )
 }
 
