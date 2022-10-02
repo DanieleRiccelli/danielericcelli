170b1562c049163f93de77cfed32cd6c02c42804
danielericcelli <danielericcelli@me.com>
2022-10-02 10:19:28 +0200

    Prod Branch

--- components/Fadein/Fadein.js
+++ components/Fadein/Fadein.js
@@ -1,6 +1,8 @@
+import styles from "./Fadein.module.css"
+
 function Fadein() {
     return (
-        <div>
+        <div className={styles.container}>
             <h1>WEEE </h1>
         </div>
     )
