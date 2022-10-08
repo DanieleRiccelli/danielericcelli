aea39d815460c5125925808dcf72d7459d999d80
danielericcelli <danielericcelli@me.com>
2022-10-08 19:01:49 +0200

    Fine prod

--- pages/page2.js
+++ pages/page2.js
@@ -5,7 +5,7 @@ export default function Page2() {
     return (
         <div className={styles.PageContainer}>
             <h2>Attualmente lavoro in</h2>
-            <a href="https://www.nextadv.it" target="_blank"><img className={styles.imageNext} src="/nextlogo.png" width={400}/></a>
+            <a href="https://www.nextadv.it" target="_blank" rel="noopener"><img className={styles.imageNext} src="/nextlogo.png" width={400}/></a>
             <h4 className={styles.onlyDesk}>e mi occupo della realizzazione di siti web,<br/> principalmente in <span className={styles.color}>Wordpress</span> e <span className={styles.color}>NextJs</span></h4>
             <h4 className={styles.onlyMob}>e mi occupo della realizzazione <br/> di siti web, principalmente in <span className={styles.color}>Wordpress</span> e <span className={styles.color}>NextJs</span></h4>
         </div>
