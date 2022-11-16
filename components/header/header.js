561648bd2c56b91acac4c2e8efdde6fc9413a5b1
Daniele <danielericcelli@me.com>
2022-11-16 10:52:24 +0100

    Fine prod

--- components/header/header.js
+++ components/header/header.js
@@ -6,7 +6,7 @@ function Header() {
                 <title>Daniele Riccelli - Web Developer</title>
                 <meta name="description" content="Il mio personale portfolio" />
                 <meta property="og:title" content="Daniele Riccelli - Web Developer" />
-                <meta property="og:description" content="Il mio personale portfolio" />
+                <meta property="og:description" content="Web Developer - NextJS - Wordpress - Angular" />
                 <meta property="og:url" content="https://www.danielericcelli.it" />
                 <meta property="og:type" content="website" />
                 <meta name="viewport" content="width=device-width, initial-scale=1" />
